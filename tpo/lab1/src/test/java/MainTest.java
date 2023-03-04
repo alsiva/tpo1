a741b089eb88216ee2e7bbb6b30452f7b155596f
alsiva <alexivanovrus@gmail.com>
2023-03-04 13:39:42 +0300

    Updated tests

--- tpo/lab1/src/test/java/MainTest.java
+++ tpo/lab1/src/test/java/MainTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -18,68 +20,52 @@ public class MainTest {
         g.addEdge(2, 3);
         g.addEdge(3, 3);
 
-        Integer result = g.main(g, 2).get(3);
-        if (result == null) {
-            fail("You should use vertex number that exists");
-        } else {
-            assertEquals(Optional.of(3), Optional.of(result));
-        }
+        List<Integer> expectedResult = Arrays.asList(2, 0, 1, 3);
+        List<Integer> realResult = g.main(g, 2);
+        assertEquals(expectedResult, realResult);
     }
 
     @Test
     public void TestFifth() {
-        Graph g = new Graph(4);
+        Graph g = new Graph(1);
+        g.addEdge(0, 0);
 
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
-
-        Integer result = g.main(g, 2).get(0);
-        if (result == null) {
-            fail("You should use vertex number that exists");
-        } else {
-            assertEquals(Optional.of(2), Optional.of(result));
-        }
+        List<Integer> expectedResult = Arrays.asList(0);
+        List<Integer> realResult = g.main(g, 0);
+        assertEquals(expectedResult, realResult);
     }
 
     @Test
     public void TestNingth() {
         Graph g = new Graph(4);
-
         g.addEdge(0, 1);
         g.addEdge(0, 2);
-        g.addEdge(1, 2);
+        g.addEdge(0, 3);
+        g.addEdge(1, 0);
         g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
+        g.addEdge(3, 0);
 
-        Integer result = g.main(g, 2).get(1);
-        if (result == null) {
-            fail("You should use vertex number that exists");
-        } else {
-            assertEquals(Optional.of(0), Optional.of(result));
-        }
+        List<Integer> expectedResult = Arrays.asList(0, 1, 2, 3);
+        List<Integer> realResult = g.main(g, 0);
+        assertEquals(expectedResult, realResult);
     }
 
     @Test
     public void TestTenth() {
-        Graph g = new Graph(4);
+        Graph g = new Graph(5);
 
         g.addEdge(0, 1);
         g.addEdge(0, 2);
-        g.addEdge(1, 2);
+        g.addEdge(1, 0);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
         g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
+        g.addEdge(3, 1);
+        g.addEdge(4, 1);
 
-        Integer result = g.main(g, 2).get(2);
-        if (result == null) {
-            fail("You should use vertex number that exists");
-        } else {
-            assertEquals(Optional.of(1), Optional.of(result));
-        }
+
+        List<Integer> expectedResult = Arrays.asList(0, 1, 3, 4, 2);
+        List<Integer> realResult = g.main(g, 0);
+        assertEquals(expectedResult, realResult);
     }
 }
